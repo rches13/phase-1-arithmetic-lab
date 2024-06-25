abf15557b7c3a5d97826b5c2d7f02b8ddf5e4585
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-06-25 12:49:24 +0300

    add arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+function generateRandomInt() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+  const random = generateRandomInt();
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % num4;
+
+const numbers = [5, 10, 15, 20];
+const max = Math.max(...numbers);
